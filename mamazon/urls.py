7a13c0a301ac50fa78de4ba142925c53dfd7f044
kashiyu <kashiyou@hotmail.co.jp>
2020-11-13 22:34:07 +0900

    lesson05

--- mamazon/urls.py
+++ mamazon/urls.py
@@ -7,4 +7,5 @@ app_name = 'mamazon'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('products/', views.ProductListView.as_view(), name='product-list')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
